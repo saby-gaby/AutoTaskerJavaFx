21485e5cadbbfa6cc1bbdd4ae8e59f5b85871a4d
saby-gaby <sgudzenova@gmail.com>
2025-07-06 15:54:25 +0200

    EditTaskController.java

--- src/main/java/com/autotasker/controller/EditTaskController.java
+++ src/main/java/com/autotasker/controller/EditTaskController.java
@@ -1,8 +1,14 @@
 package com.autotasker.controller;
 
-import com.autotasker.util.OneCheckboxSelected;
+import com.autotasker.dao.DepartmentDAO;
+import com.autotasker.dao.TaskDAO;
+import com.autotasker.dao.UserDAO;
+import com.autotasker.model.Department;
 import com.autotasker.model.Task;
-import com.autotasker.model.TaskDAO;
+import com.autotasker.model.User;
+import com.autotasker.util.OneCheckboxSelected;
+import com.autotasker.util.ComboboxUtil;
+import com.autotasker.util.WarningAlert;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
@@ -10,6 +16,8 @@ import javafx.stage.Stage;
 public class EditTaskController {
     @FXML private TextField nameField;
     @FXML private TextArea descriptionArea;
+    @FXML private ComboBox<User> userComboBox;
+    @FXML private ComboBox<Department> departmentComboBox;
     @FXML private DatePicker dueDatePicker;
     @FXML private CheckBox completedCheckBox;
     @FXML private CheckBox inProgressCheckBox;
@@ -23,6 +31,14 @@ public class EditTaskController {
         descriptionArea.setWrapText(true);
         descriptionArea.setText(task.getDescription());
 
+        ComboboxUtil.initializeAssignTaskDropdowns(new DepartmentDAO(), new UserDAO(), departmentComboBox, userComboBox);
+        if (task.getAssignedDepartment() != null) {
+            departmentComboBox.getSelectionModel().select(task.getAssignedDepartment());
+        }
+        if (task.getAssignedUser() != null) {
+            userComboBox.getSelectionModel().select(task.getAssignedUser());
+        }
+
         if (task.getDueDate() != null) dueDatePicker.setValue(task.getDueDate());
 
         completedCheckBox.setSelected(task.isCompleted());
@@ -32,10 +48,20 @@ public class EditTaskController {
 
         saveButton.setOnAction(event -> {
             task.setName(nameField.getText());
+            if (task.getName() == null || task.getName().isEmpty()) {
+                new WarningAlert("Please enter a task name.").showAndWait();
+                return;
+            }
             task.setDescription(descriptionArea.getText());
             task.setDueDate(dueDatePicker.getValue());
             task.setCompleted(completedCheckBox.isSelected());
             task.setInProgress(inProgressCheckBox.isSelected());
+            task.setAssignedDepartment(departmentComboBox.getSelectionModel().getSelectedItem());
+            task.setAssignedUser(userComboBox.getSelectionModel().getSelectedItem());
+            if (task.getAssignedUser() == null && task.getAssignedDepartment() == null) {
+                new WarningAlert("Please assign the task to a user or department.").showAndWait();
+                return;
+            }
             taskDAO.updateTask(task);
             onCloseCallback.run();
             closeDialog();
