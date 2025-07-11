f26c897115aad21d38ae8ff7db02a36919cbdb54
saby-gaby <sgudzenova@gmail.com>
2025-07-11 16:01:34 +0200

    module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,14 @@ module com.autotasker.autotasker {
     requires javafx.controls;
     requires javafx.fxml;
     requires jakarta.persistence;
+    requires jakarta.validation;
     requires org.hibernate.orm.core;
     requires java.persistence;
+    requires jbcrypt;
+    requires mysql.connector.j;
+    requires jakarta.mail;
+    requires org.jsoup;
+    requires simmetrics.core;
 
 
     opens com.autotasker.model to org.hibernate.orm.core, javafx.base;
@@ -11,4 +17,5 @@ module com.autotasker.autotasker {
     exports com.autotasker;
     exports com.autotasker.controller;
     opens com.autotasker.controller to javafx.fxml;
+    opens com.autotasker.dao to javafx.base, org.hibernate.orm.core;
 }
