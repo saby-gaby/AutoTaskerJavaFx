343fd7183005acd3940c7c572b4173c2a8f58145
saby-gaby <sgudzenova@gmail.com>
2025-08-25 15:54:41 +0200

    EditDepartmentForUserController.java

---
+++ src/main/java/com/autotasker/controller/EditDepartmentForUserController.java
@@ -0,0 +1,55 @@
+package com.autotasker.controller;
+
+import com.autotasker.dao.DepartmentDAO;
+import com.autotasker.dao.UserDAO;
+import com.autotasker.model.Department;
+import com.autotasker.model.User;
+import com.autotasker.util.ComboboxUtil;
+import com.autotasker.util.InformationAlert;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+
+public class EditDepartmentForUserController {
+
+    @FXML private TextField usernameField;
+    @FXML private ComboBox<Department> departmentComboBox;
+    @FXML private Button saveButton;
+
+    private final DepartmentDAO departmentDAO = new DepartmentDAO();
+    private final UserDAO userDAO = new UserDAO();
+
+    public void initFields(User user) {
+        ArrayList<Department> departmentsList = new ArrayList<>(departmentDAO.findAll());
+        ComboboxUtil.initializeSortedDropdown(departmentComboBox, departmentsList, Department.class);
+        // visible options for department
+        ComboboxUtil.departmentCellFactory(departmentComboBox);
+        usernameField.setText(user.getUsername());
+        usernameField.setEditable(false);
+        departmentComboBox.getSelectionModel().select(user.getDepartment());
+        saveButton.setOnAction(e -> saveNewDepartment(user));
+    }
+
+    private void saveNewDepartment(User user) {
+        user.setDepartment(departmentComboBox.getSelectionModel().getSelectedItem());
+        boolean isUpdated = userDAO.updateUser(user);
+        if (isUpdated) {
+            new InformationAlert(
+                    user.getUsername() + "'s department has been updated to '" +
+                            user.getDepartment().getDepartmentName() + "'!"
+            ).showAndWait();
+            Stage stage = (Stage) saveButton.getScene().getWindow();
+            stage.close();
+        }
+    }
+
+    @FXML
+    public void handleCancel() {
+        Stage stage = (Stage) saveButton.getScene().getWindow();
+        stage.close();
+    }
+}
